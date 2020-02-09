c0e39545ccc7bc8b20d3912908c01d55b9653c36
nkizito92 <nkizito57@gmail.com>
2020-02-09 00:09:00 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,20 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
+  # def categories_attributes=(category_attributes)
+  #   categories_attributes.values.each do |cat_att|
+  #     category = Category.find_or_create_by(cat_att)
+  #     self.categories << category
+  #   end 
+  # end 
+
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comment_attributes.build(comment: comment)
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+
+  def comment_attributes=(comment_attributes)
+    comment_attributes.values.each do |com_att|
+      comment = Comment.find_or_create_by(com_att)
+
+      self.comments << comment
+    end
+ end
 end
